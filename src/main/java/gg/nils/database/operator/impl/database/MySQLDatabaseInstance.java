2a7ec5cefac5c993314a74f7011af59da3a4116c
thenilsdev <contact@ni.ls>
2022-11-22 19:45:40 +0100

    fix: Finally?

--- src/main/java/gg/nils/database/operator/impl/database/MySQLDatabaseInstance.java
+++ src/main/java/gg/nils/database/operator/impl/database/MySQLDatabaseInstance.java
@@ -16,22 +16,24 @@ public class MySQLDatabaseInstance implements DatabaseInstance {
     public void createOrUpdate(String database, String username, String password) {
         try {
             if (!this.exists(username)) {
-                PreparedStatement preparedStatement = this.connection.prepareStatement("CREATE USER ?@% IDENTIFIED BY ?");
+                PreparedStatement preparedStatement = this.connection.prepareStatement("CREATE USER ?@? IDENTIFIED BY ?;");
                 preparedStatement.setString(1, username);
-                preparedStatement.setString(2, password);
+                preparedStatement.setString(2, "%");
+                preparedStatement.setString(3, password);
                 System.out.println(preparedStatement);
                 preparedStatement.execute();
             } else {
-                PreparedStatement preparedStatement = this.connection.prepareStatement("ALTER USER ?@% IDENTIFIED BY ?");
+                PreparedStatement preparedStatement = this.connection.prepareStatement("ALTER USER ?@? IDENTIFIED BY ?;");
                 preparedStatement.setString(1, username);
-                preparedStatement.setString(2, password);
+                preparedStatement.setString(2, "%");
+                preparedStatement.setString(3, password);
                 System.out.println(preparedStatement);
                 preparedStatement.execute();
             }
 
-            PreparedStatement preparedStatement = this.connection.prepareStatement("GRANT ALL PRIVILEGES ON ? TO ?@%;");
-            preparedStatement.setString(1, database + ".*");
-            preparedStatement.setString(2, username);
+            PreparedStatement preparedStatement = this.connection.prepareStatement("GRANT ALL PRIVILEGES ON " + database + ".* TO ?@?;");
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, "%");
             System.out.println(preparedStatement);
             preparedStatement.execute();
 
